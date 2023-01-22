f564cd5dc60ef61a1c613a09b24134070f8bb5e2
Mauricio Erazo <antonierazo13@hotmail.com>
2023-01-22 19:39:00 +0100

    update

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const colors = {
   ground: '#f4e7da',
   rock: '#d5d5d4',
   fairy: '#fceaff',
-  poison: '#98d7a5',
+  poison: '#FFA069',
   bug: '#f8d5a3',
   dragon: '#97b3e6',
   psychic: '#eaeda1',
-  flying: '#F5F5F5',
+  flying: '#AEE2FF',
   fighting: '#E6E0D4',
   normal: '#F5F5F5'
 }
 
-// Getting the colors key as a values to get [0: "fire", 1: "grass"] and so on... works to get numbers indexes.
+// Getting the colors key as a values to get [0: "fire", 1: "grass"] and so on... works to get number indexes.
 // Creating a new array with number index
 const main_types = Object.keys(colors)
 // console.log(main_types)
@@ -49,9 +49,10 @@ const createPokemonCard = (pokemon) => {
   // First letter uppercase
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
 
-  // map to create a new array just with types: we loop through and for each type, i want to return the type object (types are objects in this case) to get the type name.
+  // map to create a new array just with types: we loop through and for each type, i want to return the type object to get the type name.
   // "console.log(pokemon.type)" 
   const poke_types = pokemon.types.map(type => type.type.name)
+
   // Find the types that match with the main_types(number of list of types) and the name to show it in the pokemon card.
   // console.log(poke_types)
   const type = main_types.find(type => poke_types.indexOf(type) > -1)
